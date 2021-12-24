cbf46eda53506a67e48cb9b96bb76e2ca917ebfb
Akalanka-1996 <kasunakalanka4@gmail.com>
2021-12-24 19:26:04 +0530

    reply controller added

--- backend/controller/replyController.js
+++ backend/controller/replyController.js
@@ -2,6 +2,19 @@ const Reply = require('../model/reply.model')
 const asyncHandler = require('express-async-handler')
 const Question = require('../model/question.model')
  
+// get reply by Id
+
+const getReplyById = asyncHandler(async (req,res) => {
+    const reply = await Reply.findById(req.params.id)
+
+    if(reply) {
+        res.json(reply)
+    } else {
+        res.status(400).json({message:"Reply not found"})
+    }
+})
+
+
 // create a reply
 
 const createReply = asyncHandler(async (req,res) => {
@@ -29,4 +42,49 @@ const createReply = asyncHandler(async (req,res) => {
   
 })
 
-module.exports = {createReply}
+// update a reply
+
+const updateReply = asyncHandler(async (req,res) => {
+    const {questionId, text, image, url} = req.body
+
+    const reply = await Reply.findById(req.params.id)
+
+    if(reply.user.toString() !== req.user._id.toString()) {
+        res.status(401)
+        throw new Error("You cannot perform this action")
+    } 
+
+    if(reply) {
+        reply.text = text
+        reply.image = image
+        reply.url = url
+
+        const updatedReply = await reply.save()
+        res.json(updatedReply)
+    } else {
+        res.status(404)
+        throw new Error("Reply not found")
+    }
+})
+
+// delete a reply
+
+const deleteReply = asyncHandler(async (req,res) => {
+    const reply = await Reply.findById(req.params.id)
+
+    if(reply.user.toString() !== req.user._id.toString()) {
+        res.status(401)
+        throw new Error("You cannot perform this action")
+    } 
+
+    if(reply) {
+        await reply.remove()
+        res.json({message: "Reply removed"})
+    } else {
+        res.status(404)
+        throw new Error("Reply not found")
+    }
+
+})
+
+module.exports = {createReply, getReplyById, updateReply, deleteReply}
--- backend/routes/reply.js
+++ backend/routes/reply.js
@@ -1,8 +1,9 @@
 const router = require('express').Router()
-const {createReply} = require('../controller/replyController')
+const {createReply, getReplyById, updateReply, deleteReply} = require('../controller/replyController')
 
 const {protect} = require('../middleware/authMiddleware')
 
 router.route('/create').post(protect, createReply)
+router.route('/:id').get(getReplyById).put(protect,updateReply).delete(protect, deleteReply)
 
 module.exports = router
